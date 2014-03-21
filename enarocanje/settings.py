867217c3ecfde33083227b2ee5395f1f3e1eb19f
JonNatanael <jon.leviathan@gmail.com>
2014-03-21 21:25:01 +0100

    Changed db path, works fine.

--- enarocanje/settings.py
+++ enarocanje/settings.py
@@ -17,7 +17,7 @@ MANAGERS = ()
 DATABASES = {
 	'default': {
 		'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-		'NAME': '/var/django/enarocanje/enarocanje.db',                  # Or path to database file if using sqlite3.
+		'NAME': 'enarocanje.db',                  # Or path to database file if using sqlite3.
 		'USER': '',                       # Not used with sqlite3.
 		'PASSWORD': '',                       # Not used with sqlite3.
 		'HOST': '',                  # Set to empty string for localhost. Not used with sqlite3.
