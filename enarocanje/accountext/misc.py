e1b4c1fcdadbb815d030285294cdc8e14f370c39
pjani <pjani@TPO-Machine.(none)>
2014-04-05 09:25:54 +0200

    added and implemented MultiImageField and CustomImageField

---
+++ enarocanje/accountext/misc.py
@@ -0,0 +1,22 @@
+from django import forms
+from django.conf import settings
+from django.contrib.auth.forms import UserChangeForm as DefaultUserChangeForm
+
+from enarocanje.common.widgets import ClearableImageInput
+from models import ServiceProvider, ServiceProviderImage
+
+#TODO: MOVE THIS TO RIGHT FILE!
+class MultiImageField(forms.ImageField):
+    widget = ClearableImageInput(attrs={'multiple': 'multiple'})
+
+    def to_python(self, data):
+    
+        ret = []
+        for item in data:
+            ret.append(super(MultiImageField, self).to_python(item))
+
+        return ret
+
+class CustomImageField(forms.ImageField):
+    def to_python(self, data):
+        return super(CustomImageField, self).to_python(data[0])
