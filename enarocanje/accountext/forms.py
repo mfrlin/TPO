01e903e901a6078bc774237698651ac6ec4f725f
pjani <pjani@TPO-Machine.(none)>
2014-04-05 09:48:16 +0200

    impl size restriction validation

--- enarocanje/accountext/forms.py
+++ enarocanje/accountext/forms.py
@@ -1,13 +1,14 @@
 from django import forms
 from django.conf import settings
 from django.contrib.auth.forms import UserChangeForm as DefaultUserChangeForm
-from django.forms import ModelForm
+from django.forms import ModelForm, BaseForm, Form
 from django.utils.translation import ugettext_lazy as _
 
 from enarocanje.common.widgets import ClearableImageInput
 from enarocanje.reservations.gcal import reset_sync, sync
 from models import ServiceProvider, ServiceProviderImage
 
+from misc import MultiImageField, CustomImageField
 
 class UserChangeForm(DefaultUserChangeForm):
     phone = forms.CharField(max_length=100, label=_('Phone Number'))
@@ -54,9 +55,54 @@ class ServiceProviderForm(ModelForm):
         self.old_timezone = self.instance.timezone
 
 
-class ServiceProviderImageForm(ModelForm):
-    image = forms.ImageField(required=True, label=_('Upload an image'))
+            
 
+class ServiceProviderImageForm(ModelForm):
+    image = CustomImageField(required=True, label=_('Upload images'))
+        
     class Meta:
         model = ServiceProviderImage
         exclude = ('image_width', 'image_height', 'service_provider', 'delete_image')
+
+    def __init__(self, *args, **kwargs):
+        self.file_id = kwargs.pop('file_id', 0)
+        super(ServiceProviderImageForm, self).__init__(*args, **kwargs)
+
+    def is_valid(self):
+        if not super(ServiceProviderImageForm, self).is_valid():
+            return False
+        
+        images = self.files['image']
+        
+        if not images:
+            self.error = _("No files")
+            return False
+        
+        image = images[0]
+        
+        if image:
+            if image._size > 15*1024*1024:
+                self.error = _("Image bigger than 15MB!")
+                return False
+            
+            return True
+        else:
+            self.error = _("Image is null!")
+            return False
+
+class ServiceProviderMultiImageHelperForm(Form):
+    images = MultiImageField(required=True, label=_('Upload images'))
+        
+    def __init__(self, *args, **kwargs):
+        super(ServiceProviderMultiImageHelperForm, self).__init__(*args, **kwargs)
+        self.service_provider_forms = []
+        
+        FIELDS = args[0] if args else None
+        FILES = args[1] if args else None
+
+        if FILES:
+            if('images' in FILES):
+                for i,img in enumerate(FILES.getlist('images')):
+                    self.service_provider_forms.append(ServiceProviderImageForm(FIELDS,{'image':[img]}, file_id=i))
+
+
