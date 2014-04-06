48c36bc4590a2a63c4ce9a770c960f3ebafcb176
JonNatanael <jon.leviathan@gmail.com>
2014-04-06 23:59:38 +0200

    Added ALLOWED_HOSTS

--- enarocanje/settings.py
+++ enarocanje/settings.py
@@ -14,6 +14,8 @@ ADMINS = ()
 
 MANAGERS = ()
 
+ALLOWED_HOSTS = ('*')
+
 DATABASES = {
 'default': {
 'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
