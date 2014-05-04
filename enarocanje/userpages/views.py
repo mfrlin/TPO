c5801c0f9cab8f880c4cd0462bbc4db946e13e17
pj999 <plesnicar.jani@gmail.com>
2014-05-04 07:22:46 +0200

    completed userpage support

--- enarocanje/userpages/views.py
+++ enarocanje/userpages/views.py
@@ -19,7 +19,7 @@ from django.utils.translation import ugettext_lazy as _
 
 from enarocanje.accountext.decorators import for_service_providers
 from enarocanje.accountext.forms import ServiceProviderImageForm, ServiceProviderMultiImageHelperForm
-from enarocanje.accountext.models import ServiceProvider, ServiceProviderImage, Category as SPCategory
+from enarocanje.accountext.models import ServiceProvider, ServiceProviderImage, Category as SPCategory, User
 from enarocanje.service.models import Service, Category, Discount, Comment
 from enarocanje.reservations.models import Reservation
 
@@ -29,12 +29,42 @@ import enarocanje.common.config as config
 # List of services for editing
 
 
-def userpage_main(request, id):
-    selected_provider = get_object_or_404(ServiceProvider, id=id)
+def userpage_main(request, user_link):
+    GENERIC_GALLERY_URL = config.GENERIC_GALLERY_URL
     
+    selected_provider = get_object_or_404(ServiceProvider, userpage_link=user_link)
+    
+    provider_user = User.objects.get(service_provider=selected_provider )
+
     #location = get_location(request)
     services = Service.objects.filter(Q(active_until__gte=datetime.date.today()) | Q(active_until__isnull=True))    
-    services = services.filter(service_provider_id=id)    
+    services = services.filter(service_provider_id=selected_provider.id)    
+    
+    gallery = ServiceProviderImage.objects.filter(service_provider_id=selected_provider.id)
+    
+    categories = Category.objects.all()
+    
+    categorized_services = {}
+    
+    cats = dict(map(lambda x: (x.id,x.name), categories))
+    
+    for service in services:
+        if service.category_id in categorized_services:
+            categorized_services[service.category_id][1].append(service)
+        else:
+            categorized_services[service.category_id] \
+                = (cats[service.category_id] if service.category_id in cats else None, [service])
+    
+    service_provider_category = selected_provider.category
+    
+    generic_gallery = None
+    generic_gallery_id_name = None
+    if service_provider_category:
+        generic_gallery_id_name = service_provider_category.generic_gallery
+        if generic_gallery_id_name:
+            generic_gallery = config.GENERIC_GALLERY_IMAGES[generic_gallery_id_name] \
+                if generic_gallery_id_name in config.GENERIC_GALLERY_IMAGES \
+                else {'title': service_provider_category.name, 'values': []}
     
     working_hours = WorkingHours.objects.filter(service_provider=selected_provider);
     
@@ -60,20 +90,22 @@ def userpage_main(request, id):
 
     working_hours_blocks = []
     for k,v in wkdys.iteritems():
-        if not v:
-            continue
+        tuples = []
+        v = sorted(list(v))
+        for i in range(0,len(v),2):
+            tuples.append((v[i],v[i+1]))
     
-        working_hours_blocks.append((k,DAYS_OF_WEEK_DICT[str(k)],v))
+        working_hours_blocks.append((k,DAYS_OF_WEEK_DICT[str(k)],tuples))
     
     
-    for i in working_hours_blocks:
-        print i
+    #for i in working_hours_blocks:
+    #    print i
     #absence = Absence.objects.filter(Q(date_to__gte=datetime.date.today()) | Q(date_to__isnull=True))    
 
     lat = selected_provider.lat
     lng = selected_provider.lng
 
-    return render_to_response('userpage/provider_page.html', locals(), context_instance=RequestContext(request))
+    return render_to_response('userpages/provider_page.html', locals(), context_instance=RequestContext(request))
 
 
 
