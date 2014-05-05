5f53d4b8f39e3a9406302df09d9e7952fe2c9902
JonNatanael <jon.leviathan@gmail.com>
2014-05-05 12:47:19 +0200

    Fixed customer ui

--- enarocanje/customers/urls.py
+++ enarocanje/customers/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import patterns, url
 import views
+from enarocanje.customers.views import managecustomer
 
 urlpatterns = patterns('enarocanje.customers',
                        url(r'^mycustomers$', views.ListCustomerView.as_view(), name='mycustomers'),
                        url(r'^mycustomers/new$', views.CreateCustomerView.as_view(), name='customers-new'),
                        url(r'^mycustomers/(?P<pk>\d+)/edit$', views.EditCustomerView.as_view(), name='customers-edit'),
-
-
+                       url(r'^mycustomers/manage$', managecustomer, name='managecustomer'),
 )
