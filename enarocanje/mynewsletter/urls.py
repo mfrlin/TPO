0eb046585666b2c418810040f642b8b58449646a
ssimon92 <simon.stoiljkovikj@gmail.com>
2014-06-04 13:52:53 +0200

    Seznam obvestil strankam 1.0

--- enarocanje/mynewsletter/urls.py
+++ enarocanje/mynewsletter/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, url
+from enarocanje.mynewsletter.views import ListNewsletterView
 
 #from enarocanje.mynewsletter import views
 
@@ -6,4 +7,5 @@ urlpatterns = patterns(
     'enarocanje.mynewsletter.views',
     url(r'^mynewsletter$', 'newsletter', name='mynewsletter'),
     url(r'^mynewsletter/send$', 'send', name='sendnewsletter'),
+    url(r'^newsletterlist$', ListNewsletterView.as_view(), name='newsletterlist'),
 )
